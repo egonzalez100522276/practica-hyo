6b52f9c66886a6efec9103b2c00357ea364c883b
egonzalez100522276 <100522276@alumnos.uc3m.es>
2025-10-29 17:35:27 +0100

    remove log

--- parte-2-2/gen-2.py
+++ parte-2-2/gen-2.py
@@ -124,7 +124,7 @@ except IOError as e:
 try:
     debug_print("Ejecutando glpsol...")
     result = subprocess.run(
-        ["glpsol", "--model", "parte-2-2.mod", "--data", outfile, "-o", "output2.out", "--log", "output2.log"],
+        ["glpsol", "--model", "parte-2-2.mod", "--data", outfile, "-o", "output2.out"],
         capture_output=True,
         text=True,
         check=True,
